d09df7a4b84878af2e6bacd7b41246a3d6947f5c
a-rusak <alexander.rusak@gmail.com>
2018-08-06 03:27:45 +0300

    Cleanup, add default export

--- index.js
+++ index.js
@@ -1,241 +1,150 @@
-// import inputData from './data/input'
-const { devices, rates, maxPower } = {
-  devices: [
-    {
-      id: 'F972B82BA56A70CC579945773B6866FB',
-      name: 'Посудомоечная машина',
-      power: 950,
-      duration: 3,
-      mode: 'night'
-    },
-    {
-      id: 'C515D887EDBBE669B2FDAC62F571E9E9',
-      name: 'Духовка',
-      power: 2000,
-      duration: 2,
-      mode: 'day'
-    },
-    {
-      id: '02DDD23A85DADDD71198305330CC386D',
-      name: 'Холодильник',
-      power: 50,
-      duration: 24
-    },
-    {
-      id: '1E6276CC231716FE8EE8BC908486D41E',
-      name: 'Термостат',
-      power: 50,
-      duration: 24
-    },
-    {
-      id: '7D9DC84AD110500D284B33C82FE6E85E',
-      name: 'Кондиционер',
-      power: 850,
-      duration: 1
-    },
-    /* {
-      id: '7D9DC84AD110500D284B33C82FE6E85Z',
-      name: '_Телевизор',
-      power: 1200,
-      duration: 2
-    },
-    {
-      id: '7D9DC84AD110500D284B33C82FE6E85A',
-      name: '_Тёплый пол',
-      power: 1700,
-      duration: 3
-    },
-    {
-      id: '7D9DC84AD110500D284B33C82FE6E85B',
-      name: '_Жалюзи',
-      power: 1700,
-      duration: 3,
-      mode: 'day'
-    } */
-  ],
-  rates: [
-    {
-      from: 7,
-      to: 10,
-      value: 6.46
-    },
-    {
-      from: 10,
-      to: 17,
-      value: 5.38
-    },
-    {
-      from: 17,
-      to: 21,
-      value: 6.46
-    },
-    {
-      from: 21,
-      to: 23,
-      value: 5.38
-    },
-    {
-      from: 23,
-      to: 7,
-      value: 1.79
-    }
-  ],
-  maxPower: 2100
-};
+export default function createSchedule ({ devices = [], rates = [], maxPower = 0 } = {}) {
 
-const l = console.log;
+  const schedule = Object.assign({}, [...Array(24)].map(_ => []));
+  const powers = new Array(24).fill(0);
+  const tarifs = new Array(24).fill(0);
+  const devicesPeriod = {};
+  const dayStart = 7; // from readme
+  const nightStart = 21; // from readme
 
-const schedule = Object.assign({}, [...Array(24)].map(_ => []));
-const powers = new Array(24).fill(0);
-const tarifs = new Array(24).fill(0);
-const devicesPeriod = {};
-const dayStart = 7; // from readme
-const nightStart = 21; // from readme
+  const createPeriod = (start, duration) =>
+    [...Array(duration)].map((_, i) => (start + i) % 24);
 
-const createPeriod = (start, duration) =>
-  [...Array(duration)].map((_, i) => (start + i) % 24);
-
-rates.forEach(({ from, to, value }) => {
-  const duration = from < to ? to - from : 24 - from + to;
-  const period = createPeriod(from, duration);
-  for (let hour of period) {
-    tarifs[hour] = value;
-  }
-});
-if (tarifs.some(tarif => tarif === 0)) {
-  throw new RangeError(`Некорректно определена сетка тарифов`);
-}
-//l(tarifs)
-
-const propertyActions = [
-  {
-    name: 'day',
-    check: arg => arg >= dayStart && arg < nightStart
-  },
-  {
-    name: 'night',
-    check: arg => (arg < dayStart && arg >= 0) || arg >= nightStart
-  }
-];
-
-const getPropertyAction = arg =>
-  propertyActions.find(({ check }) => check(arg));
-
-tarifObject = tarifs.reduce((acc, tarif, hour) => {
-  return {
-    ...acc,
-    [hour]: {
-      tarif,
-      type: getPropertyAction(hour).name
-    }
-  };
-}, {});
-// l(tarifObject);
-
-/* const getDevice = (tarif, hour) => {
-  const { name } = getPropertyAction(hour);
-  return name;
-} */
-
-const getFirstCheapHour = (acc, item, index) => {
-  const [i, { tarif }] = item;
-  const compare = () => (acc[1].tarif <= tarif ? acc : item);
-  acc = index === 0 ? item : compare();
-  return acc;
-};
-
-const dayFirstCheapHour = +Object.entries(tarifObject)
-  .filter(([, { type }]) => type === 'day')
-  .reduce(getFirstCheapHour, {})[0];
-
-let nightFirstCheapHour = +Object.entries(tarifObject)
-  .filter(([, { type }]) => type === 'night')
-  .map(item => {
-    const index = item[0].length === 1 ? 30 + +item[0] : +item[0];
-    return [item[0], item[1], index];
-  })
-  .sort((a, b) => a[2] - b[2])
-  .reduce(getFirstCheapHour, {})[0];
-
-const findStartHour = ({ name, mode, power, duration }) => {
-  let attempt = 1;
-  let start = mode === 'day' ? dayFirstCheapHour : nightFirstCheapHour;
-  return testPeriod(createPeriod(start, duration));
-
-  function testPeriod(period) {
+  rates.forEach(({ from, to, value }) => {
+    const duration = from < to ? to - from : 24 - from + to;
+    const period = createPeriod(from, duration);
     for (let hour of period) {
-      if (powers[hour] + power > maxPower) {
-        if (attempt < 24 - duration) {
-          attempt++;
-          start = (start + 1) % 24;
-          // l(attempt, start, name, powers[hour]);
-          testPeriod(createPeriod(start, duration));
-        } else {
-          start = null;
-        }
-        break;
-      }
-      powers[hour] += power;
-      // l(attempt, start, name, powers[hour]);
+      tarifs[hour] = value;
     }
-    return start;
-  }
-};
-
-devices.filter(({ duration }) => duration === 24).forEach(({ id, power }) => {
-  Object.keys(schedule).forEach(hour => {
-    schedule[hour].push(id);
-    devicesPeriod[id] = createPeriod(0, 24);
-    powers[hour] += power;
   });
-});
-
-devices.filter(device => device.duration < 24).forEach(device => {
-  const { id, duration } = device;
-
-  const start = findStartHour(device);
-  if (start) {
-    // l(name, power, start, duration);
-    const period = createPeriod(start, duration);
-    devicesPeriod[id] = period;
-    for (let hour of period) {
-      schedule[hour].push(id);
-    }
-  } else {
-    throw new RangeError(
-      `Умный дом не сможет включить ${name} из-за перегрузки по мощности`
-    );
+  if (tarifs.some(tarif => tarif === 0)) {
+    throw new RangeError('Некорректно определена сетка тарифов');
   }
-});
-// l(schedule);
-// l(powers)
-// powers.map((p, i) => l(i, p, schedule[i]));
-const consumedEnergyValue = powers.reduce((acc, power, hour) => {
-  acc += power/1000*tarifs[hour];
-  // l(hour, power/1000*tarifs[hour])
-  return acc;
-}, 0)
-// l(consumedEnergyValue)
 
-const consumedEnergyDevices = Object.keys(devicesPeriod).reduce((acc, deviceId) => {
-  /* return {
-    ...acc,
-    [schedule[hour]]: 'aa'
-  }; */
-  //acc += power/1000*tarifs[hour];
-  const consumedEnergyDevice = devicesPeriod[deviceId].reduce((acc, hour) => {
-    //tarifs[hour]
-    const {power} = devices.find(({id, power}) => id === deviceId);
-    acc += power/1000*tarifs[hour];
+  const propertyActions = [
+    {
+      name: 'day',
+      check: arg => arg >= dayStart && arg < nightStart
+    },
+    {
+      name: 'night',
+      check: arg => (arg < dayStart && arg >= 0) || arg >= nightStart
+    }
+  ];
+
+  const getPropertyAction = arg =>
+    propertyActions.find(({ check }) => check(arg));
+
+  const tarifObject = tarifs.reduce((acc, tarif, hour) => {
+    return Object.assign(acc, {
+      [hour]: {
+        tarif,
+        type: getPropertyAction(hour).name
+      }
+    });
+  }, {});
+
+  const getFirstCheapHour = (acc, item, index) => {
+    const [, { tarif }] = item;
+    const compare = () => (acc[1].tarif <= tarif ? acc : item);
+    acc = index === 0 ? item : compare();
     return acc;
-    //l(hour, power);
-  }, 0);
-  // l(deviceId, consumedEnergyDevice)
-  return {
-    ...acc,
-    [deviceId]: consumedEnergyDevice
   };
-}, {});
 
-// l(devicesPeriod);
-// l(consumedEnergyDevices);
+  const dayFirstCheapHour = +Object.entries(tarifObject)
+    .filter(([, { type }]) => type === 'day')
+    .reduce(getFirstCheapHour, {})[0];
+
+  let nightFirstCheapHour = +Object.entries(tarifObject)
+    .filter(([, { type }]) => type === 'night')
+    .map(item => {
+      const index = item[0].length === 1 ? 30 + +item[0] : +item[0];
+      return [item[0], item[1], index];
+    })
+    .sort((a, b) => a[2] - b[2])
+    .reduce(getFirstCheapHour, {})[0];
+
+  const findStartHour = ({ mode, power, duration }) => {
+    let attempt = 1;
+    let start = mode === 'day' ? dayFirstCheapHour : nightFirstCheapHour;
+    return testPeriod(createPeriod(start, duration));
+
+    function testPeriod(period) {
+      for (let hour of period) {
+        if (powers[hour] + power > maxPower) {
+          if (attempt < 24 - duration) {
+            attempt++;
+            start = (start + 1) % 24;
+            testPeriod(createPeriod(start, duration));
+          } else {
+            start = null;
+          }
+          break;
+        }
+        powers[hour] += power;
+      }
+      return start;
+    }
+  };
+
+  devices.filter(({ duration }) => duration === 24).forEach(({ id, name, power }) => {
+    Object.keys(schedule).forEach(hour => {
+      if (powers[hour] + power > maxPower) {
+        throw new RangeError(
+          `Умный дом не сможет включить ${name} из-за перегрузки по мощности`
+        );
+      } else {
+        schedule[hour].push(id);
+        devicesPeriod[id] = createPeriod(0, 24);
+        powers[hour] += power;
+      }
+    });
+  });
+
+  devices.filter(device => device.duration < 24).forEach(device => {
+    const { id, name, duration } = device;
+
+    const start = findStartHour(device);
+    if (start) {
+      const period = createPeriod(start, duration);
+      devicesPeriod[id] = period;
+      for (let hour of period) {
+        schedule[hour].push(id);
+      }
+    } else {
+      throw new RangeError(
+        `Умный дом не сможет включить ${name} из-за перегрузки по мощности`
+      );
+    }
+  });
+
+  const consumedEnergyValue = Math.round(powers.reduce((acc, power, hour) => {
+    acc += (power / 1000) * tarifs[hour];
+    return acc;
+  }, 0) * 100) / 100;
+
+  const consumedEnergyDevices = Object.keys(devicesPeriod).reduce(
+    (acc, deviceId) => {
+      const consumedEnergyDevice = Math.round(devicesPeriod[deviceId].reduce(
+        (accu, hour) => {
+          const { power } = devices.find(({ id }) => id === deviceId);
+          accu += (power / 1000) * tarifs[hour];
+          return accu;
+        },
+        0
+      ) * 100) / 100;
+      return Object.assign(acc, {
+        [deviceId]: consumedEnergyDevice
+      });
+    },
+    {}
+  );
+
+  return {
+    schedule,
+    consumedEnergy: {
+      value: consumedEnergyValue,
+      devices: consumedEnergyDevices
+    }
+  };
+}
